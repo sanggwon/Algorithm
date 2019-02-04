6542bdcf6ea860c0631c131a033b2440de082a32
sanggwon <ck2570@naver.com>
2019-02-04 20:49:02 +0900

    190204 building

---
+++ month_2/20190204_building.py
@@ -0,0 +1,16 @@
+def building(T):
+   building = list(map(int, input().split()))
+   e = []
+   for i in range(int(T)-4):
+      a = building[i+2]-building[i+1]
+      b = building[i+2]-building[i]
+      c = building[i+2]-building[i+3]
+      d = building[i+2]-building[i+4]
+      if a<0 or b<0 or c<0 or d<0 :
+         pass
+      else:
+          e.append(min([a,b,c,d])) 
+   return sum(e)
+
+for i in range(10) :
+   print(f"#{i+1} {building(input())}")
