90e75e56681a373e84e4693d0eced25aa2024474
sanggwon <45960368+sanggwon@users.noreply.github.com>
2019-03-18 17:27:31 +0900

    경우의 수

--- month_3/20190316_최대상금.py
+++ month_3/20190316_최대상금.py
@@ -1,19 +1,19 @@
-# if __name__ == "__main__":
-T = int(input())
-for t in range(1, T + 1):
-    NUM, ITER = input().split()
-    ITER = int(ITER)
-    now = set()
-    now.add(NUM)
-    after = set()
-    for i in range(ITER):
-        while now:
-            s = list(str(now.pop()))
-            for i in range(len(NUM)):
-                for j in range(i+1,len(NUM)):
-                    if s[i] <= s[j] :
-                        s[i], s[j] = s[j], s[i]
-                        after.add(int("".join(s)))
-                        s[i], s[j] = s[j], s[i]
-        now, after = after, now
-    print("#", t, ' ', max(map(int, now)), sep='')
+if __name__ == "__main__":
+    T = int(input())
+    for t in range(1, T + 1):
+        NUM, ITER = input().split()
+        ITER = int(ITER)
+        now = set()
+        now.add(NUM)
+        after = set()
+        for i in range(ITER):
+            while now:
+                s = list(str(now.pop()))
+                for i in range(len(NUM)):
+                    for j in range(i+1,len(NUM)):
+                        if s[i] <= s[j] :
+                            s[i], s[j] = s[j], s[i]
+                            after.add(int("".join(s)))
+                            s[i], s[j] = s[j], s[i]
+            now, after = after, now
+        print("#", t, ' ', max(map(int, now)), sep='')
