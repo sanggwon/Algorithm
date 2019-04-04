91c4d783460e0c9f01de05012e7041ce766ed601
sanggwon <ck2570@naver.com>
2019-04-04 18:00:08 +0900

    시뮬레이션

--- month_4/[모의 SW 역량테스트] 핀볼 게임.py
+++ month_4/[모의 SW 역량테스트] 핀볼 게임.py
@@ -6,10 +6,11 @@ for tc in range(1,T):
     N = int(input().strip())
     Pinball = [list(map(int,input().strip().split())) for _ in range(N)]
     wormhole= [[] for _ in range(5)]
+    max_cnt = 0
     for i in range(N):
         for j in range(N):
             if Pinball[i][j] > 5 :
-                wormhole[Pinball[i][j]-5].append((i,j))
+                wormhole[Pinball[i][j]-6].append((i,j))
 
     for i in range(N):
         for j in range(N):
@@ -19,6 +20,18 @@ for tc in range(1,T):
                     nj = j+id_j[n]
                     cnt = 0
                     while True :
+                        if 0<=ni<N and 0<=nj<N:
+                            if Pinball[ni][nj] == -1 or (ni == i and nj == j) :
+                                break
+                            if Pinball[ni][nj] > 5 :
+                                for wi,wj in wormhole[Pinball[ni][nj]-6] :
+                                    if wi == ni and wj == nj :
+                                        if wormhole[Pinball[ni][nj]-6].index((wi,wj)) == 0:
+                                            ni, nj = wormhole[Pinball[ni][nj]-6][1]
+                                        else :
+                                            ni, nj = wormhole[Pinball[ni][nj]-6][0]
+                                        break
+
                         if n == 0:
                             if nj == N :
                                 n = 2
@@ -38,8 +51,8 @@ for tc in range(1,T):
                             elif Pinball[ni][nj] == 5:
                                 n = 2
                                 cnt += 1
-                            ni = i + id_i[n]
-                            nj = j + id_j[n]
+                            ni += id_i[n]
+                            nj += id_j[n]
 
                         elif n == 1:
                             if ni == N :
@@ -60,11 +73,11 @@ for tc in range(1,T):
                             elif Pinball[ni][nj] == 5:
                                 n = 3
                                 cnt += 1
-                            ni = i + id_i[n]
-                            nj = j + id_j[n]
+                            ni += id_i[n]
+                            nj += id_j[n]
 
                         elif n == 2:
-                            if nj == 0:
+                            if nj < 0:
                                 n = 0
                                 cnt += 1
                             elif Pinball[ni][nj] == 1:
@@ -82,11 +95,11 @@ for tc in range(1,T):
                             elif Pinball[ni][nj] == 5:
                                 n = 0
                                 cnt += 1
-                            ni = i + id_i[n]
-                            nj = j + id_j[n]
+                            ni += id_i[n]
+                            nj += id_j[n]
 
                         elif n == 3:
-                            if ni == 0:
+                            if ni < 0:
                                 n = 1
                                 cnt += 1
                             elif Pinball[ni][nj] == 1:
@@ -104,18 +117,8 @@ for tc in range(1,T):
                             elif Pinball[ni][nj] == 5:
                                 n = 1
                                 cnt += 1
-                            ni = i + id_i[n]
-                            nj = j + id_j[n]
-
-                        if Pinball[ni][nj] > 5 :
-                            for wi,wj in wormhole[Pinball[ni][nj]-5] :
-                                if wi == ni and wj == nj :
-                                    if wormhole[Pinball[ni][nj]-5].index((wi,wj)) == 0:
-                                        ni, nj = wormhole[Pinball[ni][nj]-5][1]
-                                    else :
-                                        ni, nj = wormhole[Pinball[ni][nj]-5][0]
-
-                        if Pinball[ni][nj] == -1 or (ni == i and nj == j) :
-                            break
-                        break
-    print(cnt)
+                            ni += id_i[n]
+                            nj += id_j[n]
+                    if max_cnt < cnt :
+                        max_cnt = cnt
+    print("#{} {}".format(tc,max_cnt))
