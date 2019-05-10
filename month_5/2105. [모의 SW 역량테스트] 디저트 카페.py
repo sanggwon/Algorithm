c12b99012c7e7bda7314f4a51b61a0eac6790fd5
sanggwon <45960368+sanggwon@users.noreply.github.com>
2019-05-10 17:52:39 +0900

    for문

--- month_5/2105. [모의 SW 역량테스트] 디저트 카페.py
+++ month_5/2105. [모의 SW 역량테스트] 디저트 카페.py
@@ -6,6 +6,7 @@ T = int(input())+1
 for tc in range(1,T):
     N = int(input())
     arr = [list(map(int,input().split())) for _ in range(N)]
+    max_num = 0
     for start_i in range(N-2):
         # 맨 아래면 제외
         for start_j in range(1,N-1):
@@ -13,9 +14,72 @@ for tc in range(1,T):
             next_i = start_i
             for next_j in range(start_j+1,N):
                 next_i += 1
-                if next_i == N-1:
-                    break
                 next_next_j = next_j
-                for next_next_i in range(next_i+1,next_i+1+start_j):
+                for next_next_i in range(next_i+1,N):
                     next_next_j -= 1
-                    print((start_i,start_j),(next_i,next_j),(next_next_i,next_next_j))
+
+                    if next_j-next_next_j == start_j +1:
+                        break
+
+                    last_i = start_i+(next_next_i-next_i)
+                    last_j = start_j+(next_next_j-next_j)
+
+
+                    list_num = []
+                    check = 0
+
+
+                    j = start_j
+                    for start in range(start_i,next_i) :
+                        if not arr[start][j] in list_num :
+                            list_num.append(arr[start][j])
+                            j += 1
+                        else :
+                            check = 1
+                            break
+
+                    if check :
+                        break
+
+
+                    j = next_j
+                    for next in range(next_i,next_next_i):
+                        if not arr[next][j] in list_num :
+                            list_num.append(arr[next][j])
+                            j -= 1
+                        else :
+                            check = 1
+                            break
+                    if check :
+                        break
+
+
+                    j = next_next_j
+                    for next_next in range(next_next_i,last_i,-1):
+                        if not arr[next_next][j] in list_num :
+                            list_num.append(arr[next_next][j])
+                            j -= 1
+                        else :
+                            check = 1
+                            break
+
+                    if check == 0:
+                        j = last_j
+                        for last in range(last_i, start_i,-1):
+                            if not arr[last][j] in list_num :
+                                list_num.append(arr[last][j])
+                                j += 1
+                            else :
+                                check = 1
+                                break
+
+                    if check == 0 :
+                        if max_num < len(list_num):
+                            max_num = len(list_num)
+
+    if max_num :
+        print("#{} {}".format(tc,max_num))
+    else :
+        print("#{} {}".format(tc, -1))
+
+
